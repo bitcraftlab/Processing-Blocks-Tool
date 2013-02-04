9d335e54ca50b6d7f7541bb356628cb8bae9bcf1
Martin Schneider <craftoid@k2g2.org>
2013-02-04 03:22:07 +0100

    improve build process and custom source structure
    - create missing placeholders in build.xml
    - create placeholder to insert license info as a multiline comment
    - seprate license.txt and authors.txt
    - custom package structure: bitcraftlab.blocks.tool

---
+++ src/bitcraftlab/blocks/tool/BlocksTool.java
@@ -0,0 +1,37 @@
+/**
+ *
+ * ##description##
+ *
+ * ##copyright## ##author##
+ *
+ * ##license## *
+ * 
+ * @author		##author##
+ * @modified	##date##
+ * @version		##version##
+ *
+ */
+
+ package bitcraftlab.blocks.tool;
+ 
+ import processing.app.*;
+ import processing.app.tools.*;
+
+
+ public class BlocksTool implements Tool {
+ 
+	public String getMenuTitle() {
+		return "Blocks";
+	}
+ 
+	public void init(Editor theEditor) {
+	}
+ 
+	public void run() {
+		System.out.println("Let's snap blocks together. ##name## ##version## by ##author##");
+	}
+ 
+ }
+
+
+
