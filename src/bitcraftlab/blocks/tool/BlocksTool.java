48f97df1599280859128bf467af46e89bee7a190
josh giesbrecht <josh@thoughtlost.org>
2013-02-18 22:45:43 -0800

    changing all lang_def filename refs to abs paths total hack to see if this at least works - and it does! to test this out you'll have to either modify the two filename refs to fit your local paths, or put copies of lang_def.xml and lang_def.dtd into c:\users\josh\workspace\Processing-Blocks-Tool\data to match.
    BUT! This actually builds and opens up the sample OpenBlocks panel when
    run!

--- src/bitcraftlab/blocks/tool/BlocksTool.java
+++ src/bitcraftlab/blocks/tool/BlocksTool.java
@@ -14,6 +14,8 @@
 
 package bitcraftlab.blocks.tool;
  
+import java.io.File;
+
 import edu.mit.blocks.controller.WorkspaceController;
 import processing.app.*;
 import processing.app.tools.*;
@@ -32,7 +34,9 @@ import processing.app.tools.*;
 		System.out.println("Let's snap blocks together. ##name## ##version## by ##author##");
 		
 		String[] args = new String[1];
-		args[0] = new String("/data/lang_def.xml");
+		// TODO: how the heck do we pass in the right relative path for it to load from the data folder?
+		args[0] = "C:/Users/josh/workspace/Processing-Blocks-Tool/data/lang_def.xml";
+		System.out.println(args[0]);
 		WorkspaceController.main(args);
 		
 	}
